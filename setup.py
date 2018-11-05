ddf601ae250676f34677dbee6d681abcb3f81f1f
Adam Dangoor <adamdangoor@gmail.com>
2018-11-05 01:50:08 +0000

    Progress towards installable package

---
+++ setup.py
@@ -0,0 +1,61 @@
+"""
+Setup script.
+"""
+
+from pathlib import Path
+import versioneer
+from setuptools import find_packages, setup
+from typing import List
+
+
+def _get_dependencies(requirements_file: Path) -> List[str]:
+    """
+    Return requirements from a requirements file.
+
+    This expects a requirements file with no ``--find-links`` lines.
+    """
+    lines = requirements_file.read_text().strip().split('\n')
+    return [line for line in lines if not line.startswith('#')]
+
+
+_DIRECT_REQUIRES = _get_dependencies(
+    requirements_file=Path('requirements.txt'),
+)
+
+INSTALL_REQUIRES = _DIRECT_REQUIRES
+DEV_REQUIRES = _get_dependencies(
+    requirements_file=Path('dev-requirements.txt'),
+)
+
+LONG_DESCRIPTION = Path('README.rst').read_text()
+
+setup(
+    name='DC/OS Installer Tools',
+    version=versioneer.get_version(),  # type: ignore
+    cmdclass=versioneer.get_cmdclass(),  # type: ignore
+    author='Adam Dangoor',
+    author_email='adangoor@mesosphere.com',
+    description='Get information from DC/OS installers.',
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(where='src'),
+    zip_safe=False,
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    include_package_data=True,
+    license='Apache License 2.0',
+    keywords='dcos',
+    url='https://github.com/adamtheturtle/dcos-installer-tools',
+    extras_require={
+        'dev': DEV_REQUIRES,
+    },
+    classifiers=[
+        'Operating System :: POSIX',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: Apache License 2.0',
+    ],
+    # Avoid dependency links because they are not supported by Read The Docs.
+    #
+    # Also, they require users to use ``--process-dependency-links``.
+    dependency_links=[],
+)

06260ff11ab17f0246beeb048d07b4b7ca1b3984
Adam Dangoor <adamdangoor@gmail.com>
2018-11-05 02:55:46 +0000

    Progress towards CI

---
+++ admin/run_script.py
@@ -0,0 +1,36 @@
+"""
+Run tests and linters on Travis CI.
+"""
+
+import os
+import subprocess
+import sys
+from typing import Dict  # noqa: F401
+
+import pytest
+
+
+def run_test(test_pattern: str) -> None:
+    """
+    Run pytest with a given test pattern.
+    """
+    result = pytest.main(
+        [
+            '-vvv',
+            '--exitfirst',
+            '--capture',
+            'no',
+            test_pattern,
+            '--cov=src/artifact_utils',
+            '--cov=tests',
+        ],
+    )
+    sys.exit(result)
+
+
+if __name__ == '__main__':
+    CI_PATTERN = os.environ.get('CI_PATTERN')
+    if CI_PATTERN:
+        run_test(test_pattern=CI_PATTERN)
+    else:
+        subprocess.check_call(['make', 'lint'])
